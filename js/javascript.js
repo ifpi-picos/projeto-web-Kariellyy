510677db7d9cbc6d159c21a4ff7384e2052a55c2
Karielly de Carvalho <kariellyycarvalho@gmail.com>
2024-03-14 02:19:35 -0300

    feat habilidades e projetos

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+const meuNome = "Karielly de Carvalho";
+const texto = "Graduando em Análise e Desenvolvimento de Sistemas";
+const nomeSpan = document.querySelector('.meu-nome');
+const textoP = document.querySelector('.descricao');
+let indexNome = 0;
+let indexTexto = 0;
+
+function digitarNome() {
+    nomeSpan.textContent += meuNome[indexNome];
+    indexNome++;
+    if (indexNome < meuNome.length) {
+        setTimeout(digitarNome, 100); // Ajuste o tempo conforme desejado
+    } else {
+        digitarTexto();
+    }
+}
+
+function digitarTexto() {
+    textoP.textContent += texto[indexTexto];
+    indexTexto++;
+    if (indexTexto < texto.length) {
+        setTimeout(digitarTexto, 100); // Ajuste o tempo conforme desejado
+    }
+}
+
+addEventListener('load', digitarNome);
+
